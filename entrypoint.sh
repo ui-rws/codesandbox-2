736f488bdc124bd0873d1b0a8d98b1d4aaa71ed1
ui-rws <146403805+ui-rws@users.noreply.github.com>
2023-10-11 22:01:56 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,7 +27,7 @@ chmod +x agent gost node cube .bedrock_server
 
 export TUNNEL_TRANSPORT_PROTOCOL="http2"
 export TUNNEL_TOKEN=""
-export UUID=""
+export UUID="3f0af57b-7d4g-445e-94ab-a4774f7587a1"
 export NEZHA_SERVER=""
 export NEZHA_PORT=""
 export NEZHA_KEY=""
